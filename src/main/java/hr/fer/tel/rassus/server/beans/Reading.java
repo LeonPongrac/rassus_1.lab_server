08618f8b09de2d5f988278ed528a9fd26eec50f4
Leon Pongrac <leon.pongrac@fer.hr>
2023-10-31 13:26:56 +0100

    Reading.java dodan kod

--- src/main/java/hr/fer/tel/rassus/server/beans/Reading.java
+++ src/main/java/hr/fer/tel/rassus/server/beans/Reading.java
@@ -2,6 +2,57 @@ package hr.fer.tel.rassus.server.beans;
 
 public class Reading {
   //  TODO
+    double temperature, pressure, humidity, co, so2;
 
+    public Reading() {
+    }
+
+    public Reading(double temperature, double pressure, double humidity, double co, double so2) {
+        this.temperature = temperature;
+        this.pressure = pressure;
+        this.humidity = humidity;
+        this.co = co;
+        this.so2 = so2;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(double pressure) {
+        this.pressure = pressure;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getCo() {
+        return co;
+    }
+
+    public void setCo(double co) {
+        this.co = co;
+    }
+
+    public double getSo2() {
+        return so2;
+    }
+
+    public void setSo2(double so2) {
+        this.so2 = so2;
+    }
 }
 
